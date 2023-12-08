c9e9d138c769a1f1fe0ff440623e6ead5db76a62
leo <leo.brveiere@ynov.com>
2023-12-08 10:38:07 +0100

    fixed update design and add rediction button

--- src/cv_details.php
+++ src/cv_details.php
@@ -3,9 +3,94 @@
 <head>
     <meta charset="UTF-8">
     <title>Détails du CV</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+            color: #333;
+        }
+
+        h1, h2 {
+            color: #333;
+        }
+
+        .container {
+            display: flex;
+        }
+
+        .left-column, .right-column {
+            flex: 1;
+            padding: 10px;
+        }
+
+        .left-column {
+            margin-right: 20px;
+        }
+
+        a {
+            color: #0275d8;
+            text-decoration: none;
+            display: inline-block;
+            margin-top: 10px;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .details {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            padding: 15px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .details p {
+            margin: 5px 0;
+        }
+
+        .download-link {
+            background-color: #0275d8;
+            color: white;
+            padding: 10px 15px;
+            border-radius: 5px;
+            text-transform: uppercase;
+            font-weight: bold;
+        }
+
+        .download-link:hover {
+            background-color: #025aa5;
+        }
+
+        iframe {
+            width: 100%;
+            height: 400px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        .home-button {
+            background-color: #0275d8;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            margin-top: 20px;
+            text-decoration: none; /* Supprime le soulignement du lien */
+            display: inline-block; /* Permet de définir des dimensions au lien */
+            font-weight: bold;
+        }
+
+        .home-button:hover {
+            background-color: #025aa5;
+        }
+    </style>
 </head>
 <body>
 <h1>Détails du CV</h1>
+<a href="/" class="home-button">Home</a>
 <?php
 $host = 'db';
 $db = 'cvdb';
@@ -19,55 +104,66 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-
-    if (isset($_GET['userId']) && is_numeric($_GET['userId'])) {
-        $userId = $_GET['userId'];
-
-        $stmt = $pdo->prepare("SELECT * FROM profil WHERE UserId = ?");
-        $stmt->execute([$userId]);
-        $profil = $stmt->fetch();
-
-
-        echo "<h2>Profil de " . htmlspecialchars($profil['FirstName']) . " " . htmlspecialchars($profil['LastName']) . "</h2>";
-        echo "<p>ID: " . htmlspecialchars($profil['UserId']) . "</p>";
-        echo "<p>Date de naissance: " . htmlspecialchars($profil['Birthday']) . "</p>";
-        echo "<p>Numéro de téléphone: " . htmlspecialchars($profil['PhoneNumb']) . "</p>";
-        echo "<p>Ville: " . htmlspecialchars($profil['City']) . "</p>";
-        echo "<p>Pays: " . htmlspecialchars($profil['Country']) . "</p>";
-        echo "<p>Permis: " . htmlspecialchars($profil['Permis']) . "</p>";
-        echo "<p>Description: " . htmlspecialchars($profil['Description']) . "</p>";
-
-
-        $stmt = $pdo->prepare("SELECT * FROM experience WHERE UserId = ?");
-        $stmt->execute([$userId]);
-        while ($exp = $stmt->fetch()) {
-            echo "<p>Expérience: " . htmlspecialchars($exp['ExpName']) . ", Position: " . htmlspecialchars($exp['JobPosition']) . ", De " . htmlspecialchars($exp['ExpStart']) . " à " . htmlspecialchars($exp['ExpEnd']) . ", Localisation: " . htmlspecialchars($exp['Localisation']) . ", Description: " . htmlspecialchars($exp['ExpDescription']) . "</p>";
-        }
-
-        $stmt = $pdo->prepare("SELECT * FROM skills WHERE UserId = ?");
-        $stmt->execute([$userId]);
-        while ($skill = $stmt->fetch()) {
-            echo "<p>Compétence: " . htmlspecialchars($skill['SkillName']) . ", Description: " . htmlspecialchars($skill['ExpDescription']) . "</p>";
-        }
-
-        $stmt = $pdo->prepare("SELECT * FROM certification WHERE UserId = ?");
-        $stmt->execute([$userId]);
-        while ($cert = $stmt->fetch()) {
-            echo "<p>Certification: " . htmlspecialchars($cert['CertName']) . ", Date: " . htmlspecialchars($cert['CertDate']) . ", Description: " . htmlspecialchars($cert['CertDescription']) . "</p>";
-        }
-
-    } else {
-        echo "<p>Utilisateur non spécifié ou invalide.</p>";
-    }
-
-} catch (\PDOException $e) {
-    echo "Erreur de base de données: " . $e->getMessage();
-}
-echo "<a href='/cv_pdf?userId=" . htmlspecialchars($userId) . "'>Télécharger le CV en PDF</a>
-<iframe src='/cv_pdf?userId=" . htmlspecialchars($userId) . "'></iframe>";
 ?>
+
+<div class="container">
+    <div class="left-column">
+        <div class="details">
+            <?php
+            try {
+                $pdo = new PDO($dsn, $user, $pass, $options);
+
+                if (isset($_GET['userId']) && is_numeric($_GET['userId'])) {
+                    $userId = $_GET['userId'];
+
+                    $stmt = $pdo->prepare("SELECT * FROM profil WHERE UserId = ?");
+                    $stmt->execute([$userId]);
+                    $profil = $stmt->fetch();
+
+
+                    echo "<h2>Profil de " . htmlspecialchars($profil['FirstName']) . " " . htmlspecialchars($profil['LastName']) . "</h2>";
+                    echo "<p>ID: " . htmlspecialchars($profil['UserId']) . "</p>";
+                    echo "<p>Date de naissance: " . htmlspecialchars($profil['Birthday']) . "</p>";
+                    echo "<p>Numéro de téléphone: " . htmlspecialchars($profil['PhoneNumb']) . "</p>";
+                    echo "<p>Ville: " . htmlspecialchars($profil['City']) . "</p>";
+                    echo "<p>Pays: " . htmlspecialchars($profil['Country']) . "</p>";
+                    echo "<p>Permis: " . htmlspecialchars($profil['Permis']) . "</p>";
+                    echo "<p>Description: " . htmlspecialchars($profil['Description']) . "</p>";
+
+
+                    $stmt = $pdo->prepare("SELECT * FROM experience WHERE UserId = ?");
+                    $stmt->execute([$userId]);
+                    while ($exp = $stmt->fetch()) {
+                        echo "<p>Expérience: " . htmlspecialchars($exp['ExpName']) . ", Position: " . htmlspecialchars($exp['JobPosition']) . ", De " . htmlspecialchars($exp['ExpStart']) . " à " . htmlspecialchars($exp['ExpEnd']) . ", Localisation: " . htmlspecialchars($exp['Localisation']) . ", Description: " . htmlspecialchars($exp['ExpDescription']) . "</p>";
+                    }
+
+                    $stmt = $pdo->prepare("SELECT * FROM skills WHERE UserId = ?");
+                    $stmt->execute([$userId]);
+                    while ($skill = $stmt->fetch()) {
+                        echo "<p>Compétence: " . htmlspecialchars($skill['SkillName']) . ", Description: " . htmlspecialchars($skill['ExpDescription']) . "</p>";
+                    }
+
+                    $stmt = $pdo->prepare("SELECT * FROM certification WHERE UserId = ?");
+                    $stmt->execute([$userId]);
+                    while ($cert = $stmt->fetch()) {
+                        echo "<p>Certification: " . htmlspecialchars($cert['CertName']) . ", Date: " . htmlspecialchars($cert['CertDate']) . ", Description: " . htmlspecialchars($cert['CertDescription']) . "</p>";
+                    }
+
+                } else {
+                    echo "<p>Utilisateur non spécifié ou invalide.</p>";
+                }
+
+            } catch (\PDOException $e) {
+                echo "Erreur de base de données: " . $e->getMessage();
+            }
+            ?>
+            ?>
+        </div>
+    </div>
+    <div class="right-column">
+        <a href='/cv_pdf?userId=<?= htmlspecialchars($userId) ?>' class="download-link">Télécharger le CV en PDF</a>
+        <iframe src='/cv_pdf?userId=<?= htmlspecialchars($userId) ?>'></iframe>
+    </div>
+</div>
 </body>
 </html>
--- src/user.php
+++ src/user.php
@@ -4,9 +4,67 @@
     <meta charset="UTF-8">
     <title>Liste des Utilisateurs</title>
 </head>
+<style>
+    body {
+        font-family: 'Arial', sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 20px;
+        color: #333;
+    }
+
+    h1 {
+        color: #333;
+    }
+
+    a {
+        color: #0275d8;
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    .user-list {
+        list-style: none;
+        padding: 0;
+    }
+
+    .user-list li {
+        background-color: #fff;
+        border: 1px solid #ddd;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    .header a {
+        background-color: #0275d8;
+        color: white;
+        padding: 10px 15px;
+        border-radius: 5px;
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+
+    .header a:hover {
+        background-color: #025aa5;
+    }
+</style>
 <body>
-<h1>Liste des Utilisateurs</h1>
-<a href="/new_cv">nouveau cv</a>
+<div class="header">
+    <h1>Liste des Utilisateurs</h1>
+    <a href="/new_cv">Nouveau CV</a>
+</div>
 
 <?php
 $host = 'db';  // Le nom du service MySQL dans Docker
@@ -21,18 +79,22 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-
-    $stmt = $pdo->query("SELECT UserId, FirstName, LastName FROM profil");
-    while ($row = $stmt->fetch()) {
-        echo "<p><a href='/read_cv?userId=" . htmlspecialchars($row['UserId']) . "'>ID: " . htmlspecialchars($row['UserId']) . ", Nom: " . htmlspecialchars($row['LastName']) . ", Prénom: " . htmlspecialchars($row['FirstName']) . "</a></p>";
-    }
-} catch (\PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
 ?>
+<ul class="user-list">
+    <?php
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+
+        $stmt = $pdo->query("SELECT UserId, FirstName, LastName FROM profil");
+        while ($row = $stmt->fetch()) {
+            echo "<p><a href='/read_cv?userId=" . htmlspecialchars($row['UserId']) . "'>ID: " . htmlspecialchars($row['UserId']) . ", Nom: " . htmlspecialchars($row['LastName']) . ", Prénom: " . htmlspecialchars($row['FirstName']) . "</a></p>";
+        }
+    } catch (\PDOException $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
+    ?>
+</ul>
 </body>
 </html>
 
